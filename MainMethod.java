01da59437eb13bebd3edb8646efb6cfb9b199ffb
Gourab kumar Gupta <32832817+GrvSit@users.noreply.github.com>
2022-07-15 13:12:03 +0530

    Simple LinkedList create

---
+++ MainMethod.java
@@ -0,0 +1,15 @@
+package javapackage3;
+import java.util.LinkedList;
+public class MainMethod {
+	public static void main(String[] args) {
+
+        LL ll = new LL();
+        ll.head = new Node(56);
+        Node second = new Node(30);
+        Node third = new Node(70);
+
+        ll.head.next = second;
+        second.next = third;
+        ll.show();
+    }
+}
