b6bec9520a1266ed1efda33a3d83f7b1d52897b4
Gourab kumar Gupta <32832817+GrvSit@users.noreply.github.com>
2022-07-15 13:21:00 +0530

    linked list create with Adding method

--- LL.java
+++ LL.java
@@ -2,6 +2,19 @@ package javapackage3;
 
 public class LL {
 	Node head;
+ Node tail;
+    int size;
+
+    public void addFirst(int data) {
+        Node t = new Node(data);
+        t.data = data;
+        t.next = head;
+        if (size == 0) {
+            tail = t;
+        }
+        head = t;
+        size++;
+    }
 	
 	void show() {
 		Node temp = head;
